93e98685c26c9e291c62c755e2278fddb15b1fd6
Robin de Mourat <robin.demourat@gmail.com>
2019-10-23 06:59:34 +0200

    lint code

--- src/components/MarkdownPlayer.js
+++ src/components/MarkdownPlayer.js
@@ -1,4 +1,7 @@
 import React from 'react';
 import Player from 'react-markdown';
 
-export default ( { src = '' } ) => <Player escapeHtml={false} source={ src } />;
+export default ( { src = '' } ) => ( <Player
+  escapeHtml={ false }
+  source={ src }
+                                     /> );
